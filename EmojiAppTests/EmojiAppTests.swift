3b2bb47675ae36fdadc72e9ab1ba30f12da03269
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2024-11-30 17:42:16 +0530

    Initial Commit

---
+++ EmojiAppTests/EmojiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppTests.swift
+//  EmojiAppTests
+//
+//  Created by Vaibhav Agarwal on 30/11/24.
+//
+
+import Testing
+@testable import EmojiApp
+
+struct EmojiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
